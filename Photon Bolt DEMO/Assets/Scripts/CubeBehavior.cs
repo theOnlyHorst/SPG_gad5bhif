ec3a010f16c735cb4336e509acab0a87903445d0
theOnlyHorst <buschsteffmichi@chello.at>
2019-10-01 09:23:38 +0200

    added more cool bolt stuff

---
+++ Photon Bolt DEMO/Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : Bolt.EntityBehaviour<IcoolCubeState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.coolCubeTransform,transform);
+    }
+
+    public override void SimulateOwner()
+    {
+        var speed = 4f;
+        var movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+    }
+}
---
+++ Photon Bolt DEMO/Assets/Scripts/Network Callbacks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    
+    public override void SceneLoadLocalDone(string scene)
+    {
+        var spawnPos = new Vector3(Random.Range(-4,4),0,Random.Range(-4,4));
+
+        BoltNetwork.Instantiate(BoltPrefabs.MyCube, spawnPos, Quaternion.identity);
+    }
+
+
+}
